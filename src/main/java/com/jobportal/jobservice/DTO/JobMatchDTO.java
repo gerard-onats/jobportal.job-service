50e45d85804bf8ad416501ae1a38acf602e509fe
gerard-onats <gerardvonate@gmail.com>
2025-02-18 22:23:18 +0800

    feature/lombok-dependency
    - apply lombok getter and setter annotations to models

--- src/main/java/com/jobportal/jobservice/DTO/JobMatchDTO.java
+++ src/main/java/com/jobportal/jobservice/DTO/JobMatchDTO.java
@@ -1,7 +1,12 @@
 package com.jobportal.jobservice.DTO;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.io.Serializable;
 
+@Setter
+@Getter
 public class JobMatchDTO implements Serializable {
     private Long id;
     private String title;
@@ -13,68 +18,4 @@ public class JobMatchDTO implements Serializable {
     private String base64Image;
 
     /*TODO: Add list for (Skills) */
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getCompany() {
-        return company;
-    }
-
-    public void setCompany(String company) {
-        this.company = company;
-    }
-
-    public String getLocation() {
-        return location;
-    }
-
-    public void setLocation(String location) {
-        this.location = location;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public String getTimePosted() {
-        return timePosted;
-    }
-
-    public void setTimePosted(String timePosted) {
-        this.timePosted = timePosted;
-    }
-
-    public String getBase64Image() {
-        return base64Image;
-    }
-
-    public void setBase64Image(String base64Image) {
-        this.base64Image = base64Image;
-    }
-
-    public String getCompanyDescription() {
-        return companyDescription;
-    }
-
-    public void setCompanyDescription(String companyDescription) {
-        this.companyDescription = companyDescription;
-    }
 }
